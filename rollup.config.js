8aa93c45e37bbf4798c6693d3559058ed1a287f9
Devan Kavalchek <devankav7@gmail.com>
2022-11-04 00:27:35 -0400

    Working but warning

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,37 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import {terser} from 'rollup-plugin-terser';
-import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
 
 const packageJson = require('./package.json');
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
-      name: 'react-lib',
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    commonjs(),
-    typescript({tsconfig: './tsconfig.json'}),
-    postcss(),
-    terser(),
-  ],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({tsconfig: './tsconfig.json'}),
+      postcss(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{file: 'dist/index.d.ts', format: 'esm'}],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
+  },
+];

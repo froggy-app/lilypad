b28e26dd0ffac22c3a7fa98ccdc7cd77a8b6ec21
Nick Dawson <37987430+NicholasDawson@users.noreply.github.com>
2023-02-15 11:27:03 -0500

    0.0.99 - trying to fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import styles from "rollup-plugin-styles";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import dts from "rollup-plugin-dts";
 import del from "rollup-plugin-delete";
 
@@ -23,17 +25,20 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript(),
-      postcss(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+      styles({ mode: "inject" }),
       del({ targets: "dist/*" }),
     ],
+    external: ["react", "react-dom"],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
+    external: [/\.(sass|scss|css)$/],
     plugins: [dts()],
-    external: [/\.(css|less|scss)$/, "react-dom"],
   },
 ];
